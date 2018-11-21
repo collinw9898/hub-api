4309ee9ce797fb4fc506909bff748e0d9be0f2ee
Collin Werner <collinw9898@gmail.com>
2018-11-20 22:17:56 -0500

    removed bookmark code

--- app/router.go
+++ app/router.go
@@ -1,8 +1,6 @@
 package app
 
 import (
-	"hub-api/app/bookmarks"
-
 	"github.com/gorilla/mux"
 )
 
@@ -10,19 +8,12 @@ import (
 func NewRouter() *mux.Router {
 	router := mux.NewRouter()
 
-	// Bookmarks routes
-	router.HandleFunc("/api/bookmarks", bookmarks.GetBookmarks).Methods("GET")
-	router.HandleFunc("/api/bookmarks/{id}", bookmarks.GetBookmark).Methods("GET")
-	router.HandleFunc("/api/bookmarks", bookmarks.CreateBookmark).Methods("POST")
-	router.HandleFunc("/api/bookmarks/{id}", bookmarks.UpdateBookmark).Methods("PUT")
-	router.HandleFunc("/api/bookmarks/{id}", bookmarks.DeleteBookmark).Methods("DELETE")
-
 	// Todo routes
 	router.HandleFunc("/api/todo", getTasks).Methods("GET")
-	router.HandleFunc("/api/todo/{id}", bookmarks.GetBookmark).Methods("GET")
-	router.HandleFunc("/api/todo", bookmarks.CreateBookmark).Methods("POST")
-	router.HandleFunc("/api/todo/{id}", bookmarks.UpdateBookmark).Methods("PUT")
-	router.HandleFunc("/api/todo/{id}", bookmarks.DeleteBookmark).Methods("DELETE")
+	// router.HandleFunc("/api/todo/{id}").Methods("GET")
+	// router.HandleFunc("/api/todo").Methods("POST")
+	// router.HandleFunc("/api/todo/{id}").Methods("PUT")
+	// router.HandleFunc("/api/todo/{id}").Methods("DELETE")
 
 	return router
 }
