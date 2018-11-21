cc02fbbaa38e6ac6675a60c3ce100f8bfa1b1b15
Collin Werner <collinw9898@gmail.com>
2018-11-21 01:00:31 -0500

    added createTask

--- app/todo.go
+++ app/todo.go
@@ -22,15 +22,15 @@ type tasks []task
 // Gets all the tasks in the database
 func getTasks(w http.ResponseWriter, r *http.Request) {
 	// Connect to the db
-	db, err := sql.Open("sqlite3", "./data.db")
-	checkErr(err)
+	db, openErr := sql.Open("sqlite3", "./data.db")
+	checkErr(openErr)
 	defer db.Close()
 
 	// Query to get all the tasks
-	statement, err := db.Prepare("SELECT * FROM tasks")
-	checkErr(err)
-	res, err := statement.Query()
-	checkErr(err)
+	statement, selectErr := db.Prepare("SELECT * FROM tasks")
+	checkErr(selectErr)
+	res, execErr := statement.Query()
+	checkErr(execErr)
 
 	// Insert all the tasks retrieved into a slice
 	var allTasks tasks
@@ -52,23 +52,22 @@ func getTasks(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(allTasks)
 }
 
+// Get an individual task from the database
 func getTask(w http.ResponseWriter, r *http.Request) {
 	// Connect to the db
-	db, err := sql.Open("sqlite3", "./data.db")
-	checkErr(err)
+	db, openErr := sql.Open("sqlite3", "./data.db")
+	checkErr(openErr)
 	defer db.Close()
 
 	// Query to get the specific task
 	params := mux.Vars(r)
-	statement, err := db.Prepare("SELECT * FROM tasks WHERE id=" + params["id"])
-	checkErr(err)
-	res, err := statement.Query()
-	checkErr(err)
+	statement, selectErr := db.Prepare("SELECT * FROM tasks WHERE id = ?")
+	checkErr(selectErr)
+	res := statement.QueryRow(params["id"])
 
 	// Create new task from result of query
 	var id int
 	var text string
-	res.Next() // I should probably figure out why this is needed lol
 	res.Scan(&id, &text)
 
 	newTask := task{
@@ -80,6 +79,38 @@ func getTask(w http.ResponseWriter, r *http.Request) {
 
 	// Return new task as JSON
 	json.NewEncoder(w).Encode(newTask)
+}
+
+// Create a new task
+func createTask(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var newTask task
+	decodeErr := decoder.Decode(&newTask)
+	checkErr(decodeErr)
+
+	// Connect to the db
+	db, openErr := sql.Open("sqlite3", "./data.db")
+	checkErr(openErr)
+	defer db.Close()
+
+	statement, insertErr := db.Prepare("INSERT INTO tasks(text) VALUES(?)")
+	checkErr(insertErr)
+	_, execErr := statement.Exec(newTask.Text)
+	checkErr(execErr)
+
+	db.Close()
+
+	json.NewEncoder(w).Encode(newTask)
+}
+
+// Delete a task from the database
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	// Connect to the db
+	db, err := sql.Open("sqlite3", "./data.db")
+	checkErr(err)
+	defer db.Close()
+
+	db.Close()
 }
 
 // Error handling for db interactions
